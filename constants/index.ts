fd87bde1cf208bf0a52cfe5b56597934d3f46aed
shakti06 <shaktibhujel06@gmail.com>
2023-11-08 14:10:35 +1100

    constant folder created

---
+++ constants/index.ts
@@ -0,0 +1,89 @@
+export const NAV_LINKS = [
+    { href : "/" , key :"Home" , label : "Home"},
+    { href : "/" , key :"About Us" , label : "About Us"},
+    { href : "/" , key :"Our Services" , label : "Our Services"},
+    { href : "/" , key :"Carrers" , label : "Carrers"},
+
+]
+
+export const SERVICE_ITEMS= [
+    {
+        title  : "Search Doctor",
+        icon : "/user.png",
+        description : "Search your doctor from thousands of our specialist,generalists and trusted hospitals"
+    },
+    {
+        title  : "Online Pharmacy",
+        icon : "/user.png",
+        description : "Buy your medicine with our mobile application with a simple delivery system"
+    },
+    {
+        title  : "Consultation",
+        icon : "/user.png",
+        description : "Free consultation with our trusted doctors and get the best recommendations"
+    },
+    {
+        title  : "Details Info",
+        icon : "/user.png",
+        description :  "Free consultation with our trusted doctors and get the best recommendations"
+    },
+    {
+        title  : "Emergency Care",
+        icon : "/user.png",
+        description : "You can get 24/7 urgent care for yourself or your children and your lovely family"
+    },
+    {
+        title  : "Tracking",
+        icon : "/user.png",
+        description : "Track and save your medical history and health data"
+    }
+]
+
+export const ARTICLE = [
+    {
+        image : "/detection.jpeg",
+        title : "Disease detection",
+        description : "In this case the role of health care application is very important."
+    },
+    {
+        image : "/detection.jpeg",
+        title : "Disease detection",
+        description : "In this case the role of health care application is very important."
+    },
+    {
+        image : "/detection.jpeg",
+        title : "Disease detection",
+        description : "In this case the role of health care application is very important."
+    }
+]
+
+export const FOOTER =[
+    {
+        title : "Company",
+        links : [
+            "About",
+            "Testimonials",
+            "Find a doctor",
+            "Apps"
+        ]
+    },
+    {
+        title : "Region",
+        links : [
+            "Australia",
+            "India",
+            "United Kingdom",
+            "Nepal"
+        ]
+    },
+    {
+        title : "Help",
+        links : [
+            "Help Center",
+            "Contact Support",
+            "Instructions",
+            "How it works"
+        ]
+    },
+    
+]
